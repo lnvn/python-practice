d145e4a38f949b91ec90fb25b4fb55a438344da1
Long Nguyen <faker2407@gmail.com>
2023-09-22 22:25:26 +0700

    update notebook

--- basic/3_get_current_date.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-# import sys
-
-# sys.stderr = print(" lo~i ro`i ki`a :( ")
-
-# use strftime("%Y-%m-%d %H:%M:%S") to change format of datetime
-now = datetime.now()
-print("Current date time: ", now.strftime("%Y-%m-%d %H:%M:%S"))
