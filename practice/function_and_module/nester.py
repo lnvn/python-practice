373690af0c9a9053807515e256d32844e2402be8
Long Nguyen <longnd998@gmail.com>
2023-10-04 23:07:46 +0700

    update nester

--- practice/function_and_module/nester.py
+++ practice/function_and_module/nester.py
@@ -1,8 +1,14 @@
+the_list = ["xin","chao",["toi", "ten",["la","Long"]],"toi",["den", "tu"],"bac giang"]
+
 # nester.py module
-def print_lol(the_list):
+def print_lol(the_list, level=0):
     # print all nested items
     for item in the_list:
         if isinstance(item, list):
-            print_lol(item)
+            print_lol(item, level+1)
         else:
+            for tab_stop in range(level):
+                print("\t", end='')
             print(item)
+
+print_lol(the_list)
---
+++ practice/python_namespace/first_namespace.py
@@ -0,0 +1,6 @@
+def hello_world(name):
+    print(f"hello {name}")
+
+if __name__ == "__main__":
+    name = "Long"
+    hello_world(name)
