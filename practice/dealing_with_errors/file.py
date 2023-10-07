a675f4064a217d60b59954f8c8852d26e0225ded
Long Nguyen <longnd998@gmail.com>
2023-10-07 16:21:31 +0700

    add one more case to test if we has colon in line_spoke

--- practice/dealing_with_errors/file.py
+++ practice/dealing_with_errors/file.py
@@ -1,9 +1,8 @@
 data = open('liverpool.txt')
 
-try:
-    for each_line in data:
-        role, line_spoken = each_line.split(':')
-        print(role, end='')
-        print(" Said: ", end='')
-        print(line_spoken)
-except: pass
+for each_line in data:
+    # use split(":",1) to handle issue if we have another colon in line_spoke
+    role, line_spoken = each_line.split(':', 1)
+    print(role, end='')
+    print(" Said: ", end='')
+    print(line_spoken)
