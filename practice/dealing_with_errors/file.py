ef95330be10bd3ba9154227f4c0df67f11754468
Long Nguyen <longnd998@gmail.com>
2023-10-08 23:48:26 +0700

    update Jupyter notebook for dealing_with_error section

--- practice/dealing_with_errors/file.py
+++ practice/dealing_with_errors/file.py
@@ -1,8 +1,11 @@
 data = open('liverpool.txt')
 
-for each_line in data:
-    # use split(":",1) to handle issue if we have another colon in line_spoke
-    role, line_spoken = each_line.split(':', 1)
-    print(role, end='')
-    print(" Said: ", end='')
-    print(line_spoken)
+try:
+    for each_line in data:
+        # use split(":",1) to handle issue if we have another colon in line_spoke
+        role, line_spoken = each_line.split(':')
+        print(role, end='')
+        print(" Said: ", end='')
+        print(line_spoken)
+except:
+    print("Hello World")
