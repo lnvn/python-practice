564debd02782bc71e5e54a1b6204bc6b445dc9db
Long Nguyen <longnd998@gmail.com>
2023-10-12 21:48:26 +0700

    add some more exception for code

--- practice/dealing_with_errors/file.py
+++ practice/dealing_with_errors/file.py
@@ -1,11 +1,15 @@
-data = open('liverpool.txt')
-
 try:
-    for each_line in data:
-        # use split(":",1) to handle issue if we have another colon in line_spoke
-        role, line_spoken = each_line.split(':')
-        print(role, end='')
-        print(" Said: ", end='')
-        print(line_spoken)
-except:
-    print("Hello World")
+    data = open('liverpool.txt')
+    try:
+        for each_line in data:
+            # use split(":",1) to handle issue if we have another colon in line_spoke
+            role, line_spoken = each_line.split(':')
+            print(role, end='')
+            print(" Said: ", end='')
+            print(line_spoken)
+    # handle the value error exception, print out that "values error"
+    except ValueError:
+        print("values error")
+    # handle FileNotFoundError exception
+except FileNotFoundError:
+    print("File not found!")
